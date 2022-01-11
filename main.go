faeb918630f4cad114760d21aba7fa38f0bdac97
muhsufyan <muh.sufyan.ts@gmail.com>
2022-01-11 17:11:01 +0700

    url parameter

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func main() {
 
 	router.GET("/", rootHandler)
 	router.GET("/pageke2", page2Handler)
-
+	// url parameter
+	router.GET("/item/:id", urlparamHandler)
 	router.Run(":8888")
 }
 
-// param nya berupa pointer yg mengarah ke gin.Context disimpan kedlm c
 func rootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"nama":   "author",
@@ -27,4 +27,12 @@ func page2Handler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"data": "kosong",
 	})
+}
+
+func urlparamHandler(c *gin.Context) {
+	// tangkap url parameter, dlm hal ini id, dan simpan kedlm variabel id
+	id := c.Param("id")
+
+	// kirim id tsb sbg respon (data)ke user
+	c.JSON(http.StatusOK, gin.H{"url param data": id})
 }
