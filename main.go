2cb74864983429489d71efef5aef198d1e97e720
muhsufyan <muh.sufyan.ts@gmail.com>
2022-01-11 16:38:36 +0700

    router dengan func

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// go mod init nama_projek
-// install go-gin https://github.com/gin-gonic/gin
-// go run main.go
-
 package main
 
 import (
@@ -13,21 +9,22 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		// return statusnya yaitu 200, param 2 return objek interface jd bisa kembalikan apa saja (dlm hal ini kita kembalikan data)
-		c.JSON(http.StatusOK, gin.H{
-			"nama":   "author",
-			"alamat": "nama alamat author",
-		})
-	})
+	router.GET("/", rootHandler)
+	router.GET("/pageke2", page2Handler)
 
-	router.GET("/pageke2", func(c *gin.Context) {
-		// return statusnya yaitu 200, param 2 return objek interface jd bisa kembalikan apa saja (dlm hal ini kita kembalikan data)
-		c.JSON(http.StatusOK, gin.H{
-			"data": "kosong",
-		})
-	})
-
-	// liste/running
 	router.Run(":8888")
+}
+
+// param nya berupa pointer yg mengarah ke gin.Context disimpan kedlm c
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"nama":   "author",
+		"alamat": "nama alamat author",
+	})
+}
+
+func page2Handler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"data": "kosong",
+	})
 }
